ec8b870bd904e62017ccd492f45e3098fbe2e596
humandoodlebug <humandoodlebug@gmail.com>
2016-08-13 11:16:26 +0100

    Added Verify() and Stem() methods to Dictionary. Made Dictionary and Thesaurus non-static.

--- CrosswordSolver/Logic/Dictionary.cs
+++ CrosswordSolver/Logic/Dictionary.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NHunspell;
 
 namespace Logic
 {
-    public static class Dictionary
+    public class Dictionary
     {
+        private readonly Hunspell _hunspell = new Hunspell("en_gb.aff","en_gb.dic");
 
+        public bool Verify(string word) => _hunspell.Spell(word);
+        public List<string> Stem(string word) => _hunspell.Stem(word);
+
+        ~Dictionary()
+        {
+            _hunspell.Dispose();
+        }
     }
 }
--- CrosswordSolver/Logic/Thesaurus.cs
+++ CrosswordSolver/Logic/Thesaurus.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
-    public static class Thesaurus
+    public class Thesaurus
     {
 
     }

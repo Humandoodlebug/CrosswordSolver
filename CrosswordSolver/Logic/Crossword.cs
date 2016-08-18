27d63644bae6a89b76d8bf0cb16fe2a5b1724734
Humandoodlebug <humandoodlebug@gmail.com>
2016-08-18 17:08:01 +0100

    Added properties & fields to Crossword
    Added properties and fields tp the Crossword Class.

--- CrosswordSolver/Logic/Crossword.cs
+++ CrosswordSolver/Logic/Crossword.cs
@@ -9,6 +9,18 @@ namespace Logic
     [Serializable]
     public class Crossword
     {
+        public char[,] Grid { get; }
 
+        public readonly List<Word> Words = new List<Word>();
+
+        [Serializable]
+        public struct Word
+        {
+            public enum Orientation { Down, Across }
+            public readonly int X, Y;
+            public char[] Letters;
+            public int Length { get; }
+            public Orientation Direction;
+        }
     }
 }
